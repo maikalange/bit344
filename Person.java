eb6e03c84fa964534fa10ca23c1af4b0ed42a869
Mai Kalange <joseph.nyirenda@gmail.com>
2017-03-03 00:00:47 +0200

    Create Person.java
    This is an example of a POJO. Compare this with the equivalent representation in JSON.

---
+++ Person.java
@@ -0,0 +1,13 @@
+public class Person{
+private String name;
+private int age;
+private String gender;
+private String nrc;
+public Person(String name, int age, String gender, String nrc){
+  this.name = name;
+  this.age = age;
+  this.gender = gender;
+  this.nrc = nrc;
+}
+
+}
